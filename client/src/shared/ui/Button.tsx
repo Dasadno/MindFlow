74f2367494e41514a9ec08d59b4e9a0117edb49e
Tetrabiter <noa.eskenkot@gmail.com>
2026-02-17 13:44:10 +0500

    create interface for button

--- client/src/shared/ui/Button.tsx
+++ client/src/shared/ui/Button.tsx
@@ -1,4 +1,9 @@
-const Button = ({ children }: { children: React.ReactNode }) => {
+interface ButtonProps {
+    children: React.ReactNode;
+    onClick?: () => void;
+}
+
+const Button = ({ children }: ButtonProps) => {
     return (
         <button className="px-4 py-2 rounded-lg font-semibold shadow-lg hover:shadow-xl transition-shadow bg-gradient-to-r from-primary to-secondary text-white">
             {children}
