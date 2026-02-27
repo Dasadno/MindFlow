925c1068455c97a155be5fa1c132aa5bbc710a04
Tetrabiter <noa.eskenkot@gmail.com>
2026-02-27 11:48:31 +0500

    Error boundary component done , ui ostalos dobavit

--- client/src/shared/ui/ErrorBoundary.tsx
+++ client/src/shared/ui/ErrorBoundary.tsx
@@ -1,7 +1,40 @@
-import { Component } from "react";
+import { Component, type ReactNode } from "react";
 
-class ErrorBoundary extends Component {
+// Пропс чтобы принимал дочерний компонент
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
 
+// Пропс для состояния ошибки
+interface ErrorBoundaryState {
+  error: Error | null;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  state: ErrorBoundaryState = {
+    error: null,
+  };
+  static getDerivedStateFromError(error: unknown): ErrorBoundaryState {
+    if (error instanceof Error) {
+      return { error };
+    }
+
+    return { error: new Error(String(error)) };
+  }
+
+  render(): ReactNode {
+    const { error } = this.state;
+
+    if (error) {
+      return (
+        <div>
+          <p>Seems like an error occured!</p>
+          <p>{error.message}</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
